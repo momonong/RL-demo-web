3d57c45c0e39a3d15f6f7e35b935dc3ec85a9188
momonong <morrischen2.9@gmail.com>
2023-07-28 12:38:32 +0800

    Test model1

---
+++ backend/main.py
@@ -0,0 +1,85 @@
+from fastapi import FastAPI
+from enum import Enum
+from pydantic import BaseModel
+from fastapi.responses import StreamingResponse
+from PIL import Image
+from io import BytesIO
+
+from model1 import predict_materials
+
+app = FastAPI()
+
+@app.get('/')
+async def root():
+    return {'message': 'hello'}
+
+class Model1Request(BaseModel):
+    angular_of_weaving: float
+    width_of_yarn: float
+    height_of_yarn: float
+    space: float
+    epoxy_E: float
+    epoxy_v: float
+    epoxy_yield_strength_1: float
+    epoxy_plastic_strain_1: float
+    epoxy_yield_strength_2: float
+    epoxy_plastic_strain_2: float
+    fibre_density: float
+    fibre_linear_density: float
+    fibre_E1: float
+    fibre_E2: float
+    fibre_E3: float
+    fibre_G12: float
+    fibre_G23: float
+    fibre_G13: float
+    fibre_v1: float
+    fibre_v2: float
+    fibre_v3: float
+    selected_cells: list
+
+@app.post('/model1')
+async def model1(request: Model1Request):
+    material_var = [
+        request.angular_of_weaving,
+        request.width_of_yarn,
+        request.height_of_yarn,
+        request.space,
+        request.epoxy_E,
+        request.epoxy_v,
+        request.epoxy_yield_strength_1,
+        request.epoxy_plastic_strain_1,
+        request.epoxy_yield_strength_2,
+        request.epoxy_plastic_strain_2,
+        request.fibre_density,
+        request.fibre_linear_density,
+        request.fibre_E1,
+        request.fibre_E2,
+        request.fibre_E3,
+        request.fibre_G12,
+        request.fibre_G23,
+        request.fibre_G13,
+        request.fibre_v1,
+        request.fibre_v2,
+        request.fibre_v3]
+    # 獲取選定的格子資料
+    selected_cells = request.selected_cells
+    # 將選取的網格索引轉換為整數
+    selected_cells = list(map(int, selected_cells))
+
+    # 在這裡進行相應的處理和計算
+    img_data = np.ones((1, 25))
+    # 將 img_data 中對應索引的值改為 0
+    img_data[0, selected_cells] = 0
+    img_result = predict_materials(img_data, material_var)
+
+    # read image
+    image = Image.open(BytesIO(img_result))
+    result = BytesIO()
+    # save image
+    image.save(result, "jpeg")
+    result.seek(0)
+    # response with image
+    return StreamingResponse(result, media_type="image/jpeg")
+
+async def math():
+    return 
---
+++ backend/model1.py
@@ -0,0 +1,95 @@
+import numpy as np
+import pandas as pd
+import urllib.request
+import os
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import load_model
+from sklearn import preprocessing
+
+def predict(x_img, x):
+    # 下載模型到本地
+    #model_url = 'https://drive.google.com/file/d/1Ch8kUFXHesjfTbrDRdgz_k6RSr83z4mq/view?usp=sharing'
+    #model_path = 'models/CNN_plastic.h5'
+    #urllib.request.urlretrieve(model_url, model_path)
+
+    # 載入模型
+    #model = load_model(model_path)
+    model = load_model('backend/model1/CNN_plastic.h5')
+
+    prediction = model.predict([x_img, x])
+    return prediction
+
+def plot_cruve(stress, strain):
+    plt.plot(strain[0], stress[0], label=f'Prediction', alpha=0.7)
+    plt.title("Stress Strain Curve", fontsize=20, x=0.5, y=1.03)
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.xlabel("Strain", fontsize=20, labelpad = 5)
+    plt.ylabel("Stress", fontsize=20, labelpad = 5)
+    plt.legend(loc = "best", fontsize=12)
+    plt.savefig('static/images/Prediction_Stress_Strain_Curve.png', dpi=200, bbox_inches ="tight")
+
+
+materials = ['Angle of weaving', 'Width of Yarn', 'Height of Yarn', 'Space', 
+             'Epoxy_E', 'Epoxy_v',
+             'Epoxy_Yield strength 1', 'Epoxy_Plastic strain 1', 
+             'Epoxy_Yield strength 2', 'Epoxy_Plastic strain 2', 
+             'Fibre_Density', 'Fibre_Linear density', 
+             'Fibre_E1', 'Fibre_E2', 'Fibre_E3', 
+             'Fibre_G12', 'Fibre_G23', 'Fibre_G13', 
+             'Fibre_v1', 'Fibre_v2', 'Fibre_v3']
+
+default = [90, 0.9, 0.3, 1.8, 
+           20000, 0.4, 
+           3, 0, 600, 0.3, 
+           2550, 0.00056, 
+           72000, 72000, 72000,
+           30000, 30000, 30000,
+           0.2, 0.2, 0.2]
+
+
+# 讀取data，取得fit函數
+train_df = pd.read_csv('backend/model1/plastic_data.csv')
+
+x_train_data = train_df.values[:,25:46]
+x_train_data = np.array(x_train_data)
+X_minMax = preprocessing.MinMaxScaler().fit(x_train_data)
+
+y_train_data = train_df.values[:,48:]
+y_train_data = np.array(y_train_data)
+Y_minMax = preprocessing.MinMaxScaler().fit(y_train_data)
+
+
+def predict_materials(img_data, material_var):
+    data = np.zeros((1, len(materials)))
+
+    for i in range(len(materials)):
+        if i == 0:
+            data[0][i] = float(material_var[i])-90
+        else: 
+            data[0][i] = float(material_var[i])
+    
+    data_fit = X_minMax.transform(data)
+    
+    img_data_pre = img_data.reshape(1, 5, 5, 1)
+    
+    prediction = predict(img_data_pre, data_fit)
+    prediction = Y_minMax.inverse_transform(prediction)
+    
+    stress_pred = np.zeros((prediction.shape[0], 20))
+    strain_pred = np.zeros((prediction.shape[0], 20))
+    i = 0
+    for value in prediction:
+        for j in range(prediction.shape[1]):
+            if j % 2 == 0:
+                stress_pred[i][j//2] = value[j]
+            elif j % 2 == 1:
+                strain_pred[i][j//2] = value[j]
+        i += 1
+    
+    img_result = plot_cruve(stress_pred, strain_pred)
+    return img_result
+
+material_var = default
+data = np.zeros((1, len(materials)))
+img_data = np.ones((1, 25))
